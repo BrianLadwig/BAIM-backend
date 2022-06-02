2d971ed51d52d26f925d38b06fdfcc584686ae67
Ashley Jiang <ashleyjiang4@gmail.com>
2022-06-02 13:22:55 +0200

    updated recipeRouter

--- routes/recipeRouter.js
+++ routes/recipeRouter.js
@@ -15,83 +15,66 @@ recipeRouter
               const recipes = await Recipe.find()
               res.status(200).json(recipes)
           } catch (error) {
-              res.status(404).json({ errors: [error.message] })
+              res.status(404).json({ errors: error.message })
           }
         })
         .post("/", requestValidator(postValidator), async (req, res) => {
-          const post = req.body;
-          const newPost = new Recipe(post)
-          const user = await User.findById(req.body.author)
-
-          if(!user) {
-              return res.status(404).json({ errors: ["User is not found"] })
-          }
-          
-          try {
-              await newPost.save()
-              // need to push the post to the user's post array
-              user.recipe.push(newPost)
-              await user.save()
-
-              res.status(200).json(newPost)
-          } catch (error) {
-              res.status(409).json({ errors: [error.message] })
-          }
+            try {
+                const post = req.body;
+                post.author = req.user._id // the id is in the cookie
+                const newPost = new Recipe(post)
+                const user = await User.findById(req.body.author)
+                await newPost.save()
+                // need to push the post to the user's post array
+                user.recipe.push(newPost)
+                await user.save()
+                res.status(200).json(newPost)
+            } catch (error) {
+                res.status(409).json({ errors: error.message})
+            }
         })
         .patch("/:id", requestValidator(updatedPostValidator), async (req, res) => {
-          const { id:_id } = req.params
-
-          if(!mongoose.Types.ObjectId.isValid(_id)) {
-              return res.status(404).json({ errors: ['No post with that id'] })
-          }
-          // console.log(req.body);
-          const updatedPost = await Recipe.findByIdAndUpdate(_id, req.body, { new: true })
-          res.json({message: 'Updated'})
+            const { id:_id } = req.params
+            const updatedPost = await Recipe.findByIdAndUpdate(_id, req.body, { new: true })
+            if(!updatedPost){
+                return res.status(404).json({ errors : "Post not found" })
+            }
+            res.json({message: 'Updated', updatedPost})
         })
         .delete("/:id", async (req, res) => {
-          const { id:_id } = req.params
-          const post = await Recipe.findById(_id)
-          const user = await User.findById(post.author)
-
-          if(!user) {
-              return res.status(404).json({ errors: ["User is not found"] })
-          }
-
-          if(!mongoose.Types.ObjectId.isValid(_id)) {
-              return res.status(404).json({ errors: 'No post with that id'})
-          }
-          
-          const postIndex = user.recipe.indexOf(_id)
-          user.recipe.splice(postIndex, 1)
-          await user.save()
-          await post.remove()
-          // await Post.findByIdAndDelete(_id)
-          res.json({ message: "Deleted", deleted: post })
+            try {
+                const { id:_id } = req.params
+                const post = await Recipe.findById(_id)
+                post.author = req.user._id //adding the userId from cookies
+                const user = await User.findById(post.author)
+                const postIndex = user.recipe.indexOf(_id)
+                user.recipe.splice(postIndex, 1)
+                await user.save()
+                await post.remove()
+                // await Post.findByIdAndDelete(_id)
+                res.json({ message: "Deleted", deleted: post })
+            } catch (error) {
+                res.status(404).json({ errors: error.message})
+            }
         })
         .patch("/:id/like", async (req, res) => {
-          const { id:_id } = req.params
-
-          if(!req.body.author) {
-              return res.status(401).json({ errors: "Unauthenticated"})
-          }
-
-          if(!mongoose.Types.ObjectId.isValid(_id)) {
-              return res.status(404).json({ errors: 'No post with that id'})
-          }
-
-          const post = await Recipe.findById(_id)
-
-          const index = post.likes.findIndex(id => id === String(req.body.author))
-          if(index === -1) {
-              // like
-              post.likes.push(req.body.author)
-          } else {
-              // dislike
-              post.likes = post.likes.filter(id => id !== String(req.body.author))
-          }
-
-          const updatedPost = await Recipe.findByIdAndUpdate(_id, post, { new: true })
-          res.json({message: "toggle like"})
+            try {
+                const { id:_id } = req.params
+                req.body.author = req.user._id
+                const post = await Recipe.findById(_id)
+                const index = post.likes.findIndex(id => id === String(req.body.author))
+                if(index === -1) {
+                    // like
+                    post.likes.push(req.body.author)
+                } else {
+                    // dislike
+                    post.likes = post.likes.filter(id => id !== String(req.body.author))
+                }
+                const updatedPost = await Recipe.findByIdAndUpdate(_id, post, { new: true })
+                res.json({message: "toggle like"})
+            } catch (errors) {
+                res.status(404).json({ errors : errors.message })
+            }
         })
 
 export default recipeRouter;
