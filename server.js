002ff757b9af7c21e4b463b3db847fe33774de50
Ashley Jiang <ashleyjiang4@gmail.com>
2022-07-15 11:11:39 +0200

    add cors config

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ dotenv.config();
 connect();
 const app = express();
 app.use(cors(corsOptions));
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
+//Cors Configuration - End
+
 app.use(express.json());
 app.use(cookieParser());
 
